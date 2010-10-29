a21f5304ebd23b696bc69939d2e5bf6b9a1c279b
Paul Ingles <paul@oobaloo.co.uk>
2010-10-29 11:27:43 +0100

    add new lein project

---
+++ src/simple_ring/core.clj
@@ -0,0 +1,1 @@
+(ns simple-ring.core)
---
+++ test/simple_ring/test/core.clj
@@ -0,0 +1,6 @@
+(ns simple-ring.test.core
+  (:use [simple-ring.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
